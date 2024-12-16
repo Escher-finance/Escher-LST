41da97978dc72fddd9fd915feba6bfbb4b136c84
isakfm <isak.rickyanto@gmail.com>
2024-12-16 16:48:19 +0700

    Fix dependencies and client to make sure next build can run

--- client/utils/msg.ts
+++ client/utils/msg.ts
@@ -1,16 +1,14 @@
-import {
-    MsgExecuteContract,
-  } from 'cosmjs-types/cosmwasm/wasm/v1/tx';
-  import {toUtf8} from '@cosmjs/encoding';
+import { MsgExecuteContract } from "cosmjs-types/cosmwasm/wasm/v1/tx";
+import { toUtf8 } from '@cosmjs/encoding';
 
 export const getExecuteContractMessage = (sender: string, contract_addr: string, msg: any, funds: any[]) => {
     return {
         typeUrl: '/cosmwasm.wasm.v1.MsgExecuteContract',
         value: MsgExecuteContract.fromPartial({
-        sender,
-        contract: contract_addr,
-        msg: toUtf8(JSON.stringify(msg)),
-        funds,
-        })  
+            sender,
+            contract: contract_addr,
+            msg: toUtf8(JSON.stringify(msg)),
+            funds,
+        })
     }
 };
