a63431cd0b79c465fc6710f2ad4d34da6adb0f45
isakfm <isak.rickyanto@gmail.com>
2024-12-19 16:02:17 +0700

    Add missing pages

---
+++ client/app/pages/dashboard/reward_asset.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+
+import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
+import { useState, useEffect } from "react";
+import { useGlobalContext } from "@/app/core/context";
+
+export default function RewardAsset() {
+
+    const [stakeBalance, setStakeBalance] = useState("");
+    const [lstakeBalance, setLstakeBalance] = useState("0");
+
+    const {
+        client,
+        userAddress,
+        network
+    } = useGlobalContext();
+
+    const getNativeBalance = async () => {
+        let bal = await client.getBalance(network?.contracts.reward, "stake");
+        setStakeBalance(bal.amount);
+    }
+    const getBalance = async () => {
+        const msg: any = {
+            balance: {
+                address: network?.contracts.reward
+            }
+        };
+
+        const { balance } = await client.queryContractSmart(
+            network?.contracts.cw20,
+            msg
+        );
+
+        setLstakeBalance(balance);
+    }
+
+    const loadBalance = async () => {
+        getNativeBalance();
+        getBalance();
+    }
+
+    useEffect(() => {
+        loadBalance();
+    }, [userAddress]);
+
+    return (
+        <Card className="w-full flex">
+            <CardHeader className="text-xl p-3">Reward Contract Assets</CardHeader>
+            <Divider />
+            <CardBody className="gap-1">
+                <div className="flex flex-col">
+                    <div className="p-3 text-sm">
+                        Native: {stakeBalance} stake
+                    </div>
+                    <div className="p-3 text-sm">
+                        LSToken: {lstakeBalance} lqStake
+                    </div>
+                </div>
+            </CardBody>
+        </Card>
+    );
+}
