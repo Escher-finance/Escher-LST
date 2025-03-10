1d1384869a195f8d15a280b7e6bcefa1fa7c6b79
Isak Rickyanto <isak.rickyanto@gmail.com>
2025-03-10 10:05:21 +0700

    Add required babylon rust proto and remove unused crate

---
+++ contracts/liquidstaking/liquidstaking-solo/src/gen/babylon.epoching.v1.rs
@@ -0,0 +1,23 @@
+/// MsgWrappedDelegate is the message for delegating stakes
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct MsgWrappedDelegate {
+    #[prost(message, optional, tag = "1")]
+    pub msg: ::core::option::Option<super::super::super::cosmos::staking::v1beta1::MsgDelegate>,
+}
+/// MsgWrappedDelegate is the response to the MsgWrappedDelegate message
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct MsgWrappedDelegateResponse {}
+/// MsgWrappedUndelegate is the message for undelegating stakes
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct MsgWrappedUndelegate {
+    #[prost(message, optional, tag = "1")]
+    pub msg: ::core::option::Option<super::super::super::cosmos::staking::v1beta1::MsgUndelegate>,
+}
+/// MsgWrappedUndelegateResponse is the response to the MsgWrappedUndelegate
+/// message
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct MsgWrappedUndelegateResponse {}
---
+++ contracts/liquidstaking/liquidstaking-solo/src/gen/mod.rs
@@ -0,0 +1,24 @@
+pub mod babylon {
+    pub mod epoching {
+        pub mod v1 {
+            include!("babylon.epoching.v1.rs");
+        }
+    }
+}
+pub mod cosmos {
+    pub mod base {
+        pub mod v1beta1 {
+            include!("cosmos.base.v1beta1.rs");
+        }
+    }
+    pub mod staking {
+        pub mod v1beta1 {
+            include!("cosmos.staking.v1beta1.rs");
+        }
+    }
+}
+pub mod google {
+    pub mod api {
+        include!("google.api.rs");
+    }
+}
