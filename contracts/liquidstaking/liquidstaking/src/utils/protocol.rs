8855522af6613bb52403679fe51e31746a6d0ad4
Nuno David <ndavidq0@gmail.com>
2025-04-01 15:10:41 +0100

    test: unittest protocol utils

--- contracts/liquidstaking/liquidstaking/src/utils/protocol.rs
+++ contracts/liquidstaking/liquidstaking/src/utils/protocol.rs
@@ -38,3 +38,40 @@ pub fn ucs03_transfer(
         funds,
     });
 }
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::from_json;
+
+    use super::*;
+
+    #[test]
+    fn test_ucs03_transfer() {
+        let ucs03_contract_addr = "ucs03".to_string();
+        let ucs03_funds = Vec::from([Coin::new(Uint128::new(100), "denom")]);
+        let result = ucs03_transfer(
+            Timestamp::default(),
+            ucs03_contract_addr.clone(),
+            u32::default(),
+            Bytes::default(),
+            String::default(),
+            Uint128::default(),
+            Bytes::default(),
+            Uint256::default(),
+            ucs03_funds.clone(),
+            H256::default(),
+        );
+        if let WasmMsg::Execute {
+            contract_addr,
+            msg,
+            funds,
+        } = result.unwrap()
+        {
+            assert_eq!(contract_addr, ucs03_contract_addr);
+            assert_eq!(funds, ucs03_funds);
+            from_json::<Ucs03ExecuteMsg>(msg).unwrap();
+        } else {
+            panic!("not WasmMsg::Execute");
+        }
+    }
+}
