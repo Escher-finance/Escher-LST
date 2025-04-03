8aa852114a7ad331842aa2607fe9b6a5ed6b23cd
Nuno David <ndavidq0@gmail.com>
2025-04-03 12:46:02 +0100

    fix: correct calculation

--- contracts/liquidstaking/liquidstaking/src/utils/calc.rs
+++ contracts/liquidstaking/liquidstaking/src/utils/calc.rs
@@ -57,11 +57,12 @@ pub fn check_slippage(
 }
 
 pub fn calculate_query_bounds(min: Option<u64>, max: Option<u64>) -> (u64, u64) {
-    let max_dist = 50;
+    // NOTE: 49 because both bounds are inclusive
+    let max_dist = 49;
     let min_bound = min.unwrap_or(1);
     let max_bound = match max {
-        Some(max) => max.min(min.unwrap_or_default() + max_dist),
-        None => min.unwrap_or_default() + max_dist,
+        Some(max) => max.min(min_bound + max_dist),
+        None => min_bound + max_dist,
     };
     (min_bound, max_bound)
 }
@@ -73,11 +74,11 @@ mod tests {
     #[test]
     fn test_calculate_query_bounds() {
         assert_eq!(calculate_query_bounds(None, None), (1, 50));
-        assert_eq!(calculate_query_bounds(Some(200), None), (200, 250));
+        assert_eq!(calculate_query_bounds(Some(200), None), (200, 249));
         assert_eq!(calculate_query_bounds(None, Some(200)), (1, 50));
-        assert_eq!(calculate_query_bounds(Some(100), Some(300)), (100, 150));
+        assert_eq!(calculate_query_bounds(Some(100), Some(300)), (100, 149));
         assert_eq!(calculate_query_bounds(Some(2), Some(10)), (2, 10));
-        assert_eq!(calculate_query_bounds(Some(1000), Some(2000)), (1000, 1050));
+        assert_eq!(calculate_query_bounds(Some(1000), Some(2000)), (1000, 1049));
         assert_eq!(calculate_query_bounds(Some(200), Some(210)), (200, 210));
     }
 }
