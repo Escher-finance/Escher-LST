c95e604f630e31dcf54b026bb444ad3996b1ded6
Nuno David <ndavidq0@gmail.com>
2025-04-03 22:14:16 +0100

    feat: add batchid to query unbond record; cleanup logic

--- contracts/liquidstaking/liquidstaking/src/tests/query.rs
+++ contracts/liquidstaking/liquidstaking/src/tests/query.rs
@@ -4,7 +4,8 @@ use crate::{query::*, ContractError};
 fn test_query_unbond_record_should_return_err_if_invalid_query() {
     use cosmwasm_std::testing::mock_dependencies;
     let deps = mock_dependencies();
-    let err = query_unbond_record(&deps.storage, None, None, None, Some(0), Some(100)).unwrap_err();
+    let err =
+        query_unbond_record(&deps.storage, None, None, None, None, Some(0), Some(100)).unwrap_err();
     let has_right_error = if let ContractError::InvalidUnbondRecordQuery {} = err {
         true
     } else {
