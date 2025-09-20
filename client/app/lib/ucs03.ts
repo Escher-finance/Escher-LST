1607e6ea75d0d4f5b83dae9fc5edb1c2d2b3c58a
Isak Rickyanto <isak.rickyanto@gmail.com>
2025-09-20 16:03:51 +0700

    Add tokenorderv2 function with initialize kind

--- client/app/lib/ucs03.ts
+++ client/app/lib/ucs03.ts
@@ -1,6 +1,6 @@
 
-import { TokenOrderV2Abi, TokenOrderV2, Call, CallAbi, Batch, Schema as Ucs03Schema, InstructionAbi, Instruction } from "@unionlabs/sdk/Ucs03";
-import { Address, encodeAbiParameters, Hex, toHex } from "viem";
+import { TokenOrderV2Abi, TokenOrderV2, Call, CallAbi, Batch, Schema as Ucs03Schema, InstructionAbi, Instruction, TokenMetadataAbi } from "@unionlabs/sdk/Ucs03";
+import { Address, encodeAbiParameters, encodePacked, encodeFunctionData, Hex, toHex, parseAbi } from "viem";
 import { ChainRegistry } from "@unionlabs/sdk/ChainRegistry";
 import { UniversalChainId } from "@unionlabs/sdk/schema/chain";
 import { TokenOrder, Ucs03, Ucs05, Utils } from "@unionlabs/sdk";
@@ -8,15 +8,23 @@ import { Effect, pipe, Schema } from "effect";
 import { getTimeoutInNanoseconds7DaysFromNow } from "@/app/lib/utils";
 import { ChannelId } from "@unionlabs/sdk/schema/channel";
 
+
+
 export const U_FROM_UNION_SOLVER_METADATA_TESTNET = "0x000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000014ba5ed44733953d79717f6269357c77718c8ba5ed0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 export const EU_FROM_UNION_SOLVER_METADATA_TESTNET = "0x000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000014e5cf13c84c0fea3236c101bd7d743d30366e5cf10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 
+const TOKEN_ORDER_KIND_INITIALIZE = 0;
 const TOKEN_ORDER_KIND_ESCROW = 1;
 const TOKEN_ORDER_KIND_SOLVE = 3;
 const TOKEN_ORDER_V2_VERSION = 2;
 export const OP_CODE_CALL = 1;
 const OP_CODE_TOKEN_ORDER_V2 = 3;
 export const INSTR_VERSION_ZERO = 0;
+
+export interface TokenMetadata {
+    implementation: `0x${string}` // bytes type in Solidity
+    initializer: `0x${string}`    // bytes type in Solidity
+}
 
 
 export const tokenOrderV2WithSolverMetadata = (
@@ -50,6 +58,12 @@ export const tokenOrderV2WithSolverMetadata = (
     return tokenOrderV2;
 }
 
+function encodeTokenMetadataWithAbi(metadata: TokenMetadata): `0x${string}` {
+    return encodeAbiParameters(
+        TokenMetadataAbi(),
+        [metadata.implementation, metadata.initializer]
+    )
+}
 
 export const tokenOrderV2Escrow = (
     sender: string,
@@ -62,6 +76,7 @@ export const tokenOrderV2Escrow = (
     let senderHex = sender.startsWith("0x") ? sender as Hex : toHex(sender);
     let receiverHex = receiver.startsWith("0x") ? receiver as Hex : toHex(receiver);
     let baseTokenHex = baseToken.startsWith("0x") ? baseToken as Hex : toHex(baseToken);
+
 
     let tokenOrderV2: TokenOrderV2 = TokenOrderV2.make({
         opcode: OP_CODE_TOKEN_ORDER_V2,
@@ -79,6 +94,84 @@ export const tokenOrderV2Escrow = (
 
     return tokenOrderV2;
 }
+
+
+
+export const tokenOrderV2Initialize = (
+    sender: string,
+    receiver: string,
+    baseToken: string,
+    amount: bigint,
+    quoteToken: `0x${string}`,
+) => {
+
+    let senderHex = sender.startsWith("0x") ? sender as Hex : toHex(sender);
+    let receiverHex = receiver.startsWith("0x") ? receiver as Hex : toHex(receiver);
+    let baseTokenHex = baseToken.startsWith("0x") ? baseToken as Hex : toHex(baseToken);
+
+    const initializerCallData = encodeZkgmERC20Initialize({
+        authority: '0x40cdff51ae7487e0b4a4d6e5f86eb15fb7c1d9f4',
+        zkgm: '0x5fbe74a283f7954f10aa04c2edf55578811aeb03',
+        name: 'eBABY',
+        symbol: 'ebbn',
+        decimals: 6,
+    })
+
+    const implementation = encodePacked(['address'],
+        ['0xAf739F34ddF951cBC24fdbBa4f76213688E13627']);
+
+
+    let tokenMetadata: TokenMetadata = {
+        implementation,
+        initializer: initializerCallData
+    };
+
+    let tokenMetadataBytes = encodeTokenMetadataWithAbi(tokenMetadata);
+
+    let tokenOrderV2: TokenOrderV2 = TokenOrderV2.make({
+        opcode: OP_CODE_TOKEN_ORDER_V2,
+        version: TOKEN_ORDER_V2_VERSION,
+        operand: [
+            senderHex,
+            receiverHex,
+            baseTokenHex,
+            amount,
+            quoteToken,
+            amount,
+            TOKEN_ORDER_KIND_INITIALIZE,
+            tokenMetadataBytes]
+    });
+
+    return tokenOrderV2;
+}
+
+export const zkgmERC20Abi = parseAbi([
+    'function initialize(address authority, address minter, string name, string symbol, uint8 decimals)'
+]);
+
+export interface ZkgmERC20InitializeParams {
+    authority: `0x${string}`
+    zkgm: `0x${string}`
+    name: string
+    symbol: string
+    decimals: number
+}
+
+
+export function encodeZkgmERC20Initialize(params: ZkgmERC20InitializeParams): `0x${string}` {
+    return encodeFunctionData({
+        abi: zkgmERC20Abi,
+        functionName: 'initialize',
+        args: [
+            params.authority,
+            params.zkgm,
+            params.name,
+            params.symbol,
+            params.decimals,
+        ],
+    })
+}
+
 
 
 export const encodeTokenOrderV2 = (instruction: TokenOrderV2) => {
