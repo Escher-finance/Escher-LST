1350f522ad0717289e32b862eb3a2dee2b43a358
Nuno David <ndavidq0@gmail.com>
2025-09-22 14:44:22 +0100

    feat: make it ownable2step

--- solidity/defi-vaults/src/EulerVault.sol
+++ solidity/defi-vaults/src/EulerVault.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract UniVault is ERC4626, Ownable {
+contract UniVault is ERC4626, Ownable2Step {
     constructor(address _owner, string memory _shareName, string memory _shareSymbol, IERC20 _underlyingAsset)
         ERC20(_shareName, _shareSymbol)
         ERC4626(_underlyingAsset)
