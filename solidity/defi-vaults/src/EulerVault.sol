32c2eda0249f3810b9e198d14d7d6124df43a412
Nuno David <ndavidq0@gmail.com>
2025-09-22 17:36:29 +0100

    feat: setup hooks

--- solidity/defi-vaults/src/EulerVault.sol
+++ solidity/defi-vaults/src/EulerVault.sol
@@ -37,6 +37,78 @@ contract EulerVault is ERC4626, Ownable2Step {
         return total;
     }
 
+    /**
+     * @dev See {IERC4626-deposit}.
+     */
+    function deposit(uint256 assets, address receiver) public virtual override returns (uint256) {
+        uint256 maxAssets = maxDeposit(receiver);
+        if (assets > maxAssets) {
+            revert ERC4626ExceededMaxDeposit(receiver, assets, maxAssets);
+        }
+
+        uint256 shares = previewDeposit(assets);
+        _deposit(_msgSender(), receiver, assets, shares);
+        _afterDeposit(assets);
+
+        return shares;
+    }
+
+    /**
+     * @dev See {IERC4626-mint}.
+     */
+    function mint(uint256 shares, address receiver) public virtual override returns (uint256) {
+        uint256 maxShares = maxMint(receiver);
+        if (shares > maxShares) {
+            revert ERC4626ExceededMaxMint(receiver, shares, maxShares);
+        }
+
+        uint256 assets = previewMint(shares);
+        _deposit(_msgSender(), receiver, assets, shares);
+        _afterDeposit(assets);
+
+        return assets;
+    }
+
+    /**
+     * @dev See {IERC4626-withdraw}.
+     */
+    function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256) {
+        uint256 maxAssets = maxWithdraw(owner);
+        if (assets > maxAssets) {
+            revert ERC4626ExceededMaxWithdraw(owner, assets, maxAssets);
+        }
+
+        uint256 shares = previewWithdraw(assets);
+        _beforeWithdraw(assets);
+        _withdraw(_msgSender(), receiver, owner, assets, shares);
+
+        return shares;
+    }
+
+    /**
+     * @dev See {IERC4626-redeem}.
+     */
+    function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256) {
+        uint256 maxShares = maxRedeem(owner);
+        if (shares > maxShares) {
+            revert ERC4626ExceededMaxRedeem(owner, shares, maxShares);
+        }
+
+        uint256 assets = previewRedeem(shares);
+        _beforeWithdraw(assets);
+        _withdraw(_msgSender(), receiver, owner, assets, shares);
+
+        return assets;
+    }
+
+    function _afterDeposit(uint256 assets) internal {
+        // TODO
+    }
+
+    function _beforeWithdraw(uint256 assets) internal {
+        // TODO
+    }
+
     function _updateEulerVault(IEVault _eulerVault) private {
         if (asset() != _eulerVault.asset()) {
             revert EscherVault_InvalidEulerVault();
