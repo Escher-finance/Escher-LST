46e70a3aa018dc3df9bfcfac2c84c14c1141531a
Nuno David <ndavidq0@gmail.com>
2025-09-22 17:26:32 +0100

    feat: connect euler vault

--- solidity/defi-vaults/src/EulerVault.sol
+++ solidity/defi-vaults/src/EulerVault.sol
@@ -5,11 +5,30 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {IEVault} from "./interfaces/IEVault.sol";
 
-contract UniVault is ERC4626, Ownable2Step {
-    constructor(address _owner, string memory _shareName, string memory _shareSymbol, IERC20 _underlyingAsset)
-        ERC20(_shareName, _shareSymbol)
-        ERC4626(_underlyingAsset)
-        Ownable(_owner)
-    {}
+contract EulerVault is ERC4626, Ownable2Step {
+    IEVault public s_eulerVault;
+
+    error EscherVault_InvalidEulerVault();
+
+    event EulerVaultUpdated(address indexed _newEulerVault);
+
+    constructor(
+        address _owner,
+        string memory _shareName,
+        string memory _shareSymbol,
+        IERC20 _underlyingAsset,
+        IEVault _eulerVault
+    ) ERC20(_shareName, _shareSymbol) ERC4626(_underlyingAsset) Ownable(_owner) {
+        _updateEulerVault(_eulerVault);
+    }
+
+    function _updateEulerVault(IEVault _eulerVault) private {
+        if (asset() != _eulerVault.asset()) {
+            revert EscherVault_InvalidEulerVault();
+        }
+        s_eulerVault = _eulerVault;
+        emit EulerVaultUpdated(address(_eulerVault));
+    }
 }
