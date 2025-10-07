09da38e5b1a41651f50f5208551f261f9f06db1e
Isak Rickyanto <isak.rickyanto@gmail.com>
2025-10-07 09:05:28 +0700

    Fix clippy warnings

--- cosmwasm/babylon-lst/contracts/liquidstaking/babylon/src/utils/calc.rs
+++ cosmwasm/babylon-lst/contracts/liquidstaking/babylon/src/utils/calc.rs
@@ -4,21 +4,24 @@ use cosmwasm_std::{Decimal, QuerierWrapper, StdResult, Uint128, Uint256};
 use cw20::TokenInfoResponse;
 
 use crate::{
+    state::{BurnQueue, MintQueue, SupplyQueue, WithdrawRewardQueue, SUPPLY_QUEUE},
     ContractError,
-    state::{BurnQueue, MintQueue, SUPPLY_QUEUE, SupplyQueue, WithdrawRewardQueue},
 };
 
 /// return how much staking token from underlying native coin denom
+#[must_use]
 pub fn calculate_staking_token_from_rate(stake_amount: Uint128, exchange_rate: Decimal) -> Uint128 {
     (Decimal::from_ratio(stake_amount, Uint128::one()) / exchange_rate).to_uint_floor()
 }
 
 /// return how much fee from reward
+#[must_use]
 pub fn calculate_fee_from_reward(reward: Uint128, fee_rate: Decimal) -> Uint128 {
     (fee_rate * Decimal::from_ratio(reward, Uint128::one())).to_uint_floor()
 }
 
 /// return how much underlying native coin denom from staking token base on exchange rate
+#[must_use]
 pub fn calculate_native_token_from_staking_token(
     staking_token: Uint128,
     exchange_rate: Decimal,
@@ -80,14 +83,16 @@ pub fn check_slippage_with_min_mint_amount(
     Ok(())
 }
 
+#[must_use]
 pub fn get_last_epoch_block(block: u64, epoch_period: u32) -> u64 {
-    let remainder: u64 = block % epoch_period as u64;
+    let remainder: u64 = block % u64::from(epoch_period);
     block - remainder
 }
 
+#[must_use]
 pub fn get_next_epoch(block: u64, epoch_period: u32) -> u64 {
-    let remainder: u64 = block % epoch_period as u64;
-    block + (epoch_period as u64 - remainder)
+    let remainder: u64 = block % u64::from(epoch_period);
+    block + (u64::from(epoch_period) - remainder)
 }
 
 fn get_elements_by_indices<T: Clone>(vec: &[T], indices: &[usize]) -> Vec<T> {
@@ -113,13 +118,14 @@ pub fn normalize_supply_queue(supply_queue: &mut SupplyQueue, current_block: u64
     let mut burn_retain: Vec<usize> = vec![];
     for (pos, burn) in supply_queue.burn.iter().enumerate() {
         if burn.block > last_epoch_block {
-            burn_retain.push(pos)
+            burn_retain.push(pos);
         }
     }
     supply_queue.mint = get_elements_by_indices(&supply_queue.mint, &mint_retain);
     supply_queue.burn = get_elements_by_indices(&supply_queue.burn, &burn_retain);
 }
 
+#[must_use]
 pub fn normalize_total_supply(
     current_supply: Uint128,
     mint_queue: &Vec<MintQueue>,
@@ -166,6 +172,8 @@ pub fn normalize_reward_balance(
     Ok(new_balance)
 }
 
+/// process the withdraw reward queue and return the new reward balance and new queue
+#[must_use]
 pub fn normalize_withdraw_reward_queue(
     current_block: u64,
     current_reward_balance: Uint128,
@@ -178,7 +186,7 @@ pub fn normalize_withdraw_reward_queue(
 
     for withdraw_reward in withdraw_reward_queue {
         if withdraw_reward.block > last_epoch_block {
-            new_queue.push(withdraw_reward)
+            new_queue.push(withdraw_reward);
         } else {
             // if the height of queue is lower than last epoch then the withdraw amount will be assumed already processed
             // and we only take the biggest amount as we only care the "last" total withdraw reward amount
@@ -193,6 +201,7 @@ pub fn normalize_withdraw_reward_queue(
 }
 
 /// return how much is the exchange rate
+#[must_use]
 pub fn calculate_exchange_rate(
     total_bond_amount: Uint128,
     total_supply: Uint128,
@@ -206,6 +215,7 @@ pub fn calculate_exchange_rate(
     exchange_rate
 }
 
+#[must_use]
 pub fn calculate_query_bounds(min: Option<u64>, max: Option<u64>) -> (u64, u64) {
     // NOTE: 49 because both bounds are inclusive
     let max_dist = 49;
@@ -217,6 +227,7 @@ pub fn calculate_query_bounds(min: Option<u64>, max: Option<u64>) -> (u64, u64) 
     (min_bound, max_bound)
 }
 
+#[must_use]
 pub fn calculate_dust_distribution(dust_amount: Uint128, receivers_len: Uint128) -> Vec<Uint128> {
     if receivers_len.is_zero() {
         return Vec::new();
