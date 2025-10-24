12afef1f2782ba4b2b351aefcc783f707442105b
Isak Rickyanto <isak.rickyanto@gmail.com>
2025-10-24 10:09:32 +0700

    Refactor send back unstaked native token on withdrawal

--- cosmwasm/babylon-lst/contracts/liquidstaking/babylon/src/execute.rs
+++ cosmwasm/babylon-lst/contracts/liquidstaking/babylon/src/execute.rs
@@ -872,6 +872,7 @@ pub fn process_batch_withdrawal(
                 };
 
                 // send native token back via ucs03
+                // no need to increase allowance as we send native denom
                 let msg = Ucs03Zkgm::new(
                     ucs03_relay_contract.clone(),
                     TokenPair {
@@ -879,14 +880,20 @@ pub fn process_batch_withdrawal(
                         quote_token: quote_token.quote_token.clone(),
                     },
                 )
-                .transfer_escrow(&ZkgmTransfer {
-                    sender: lst_contract.to_string(),
-                    amount: unstake_return_native_amount,
-                    recipient: recipient.clone(),
-                    recipient_channel_id,
-                    salt,
-                    time,
-                })?;
+                .transfer_escrow_with_funds(
+                    &ZkgmTransfer {
+                        sender: lst_contract.to_string(),
+                        amount: unstake_return_native_amount,
+                        recipient: recipient.clone(),
+                        recipient_channel_id,
+                        salt,
+                        time,
+                    },
+                    &[Coin {
+                        amount: unstake_return_native_amount,
+                        denom: denom.clone(),
+                    }], // need to attach funds to send unstaked token which is based on native denom
+                )?;
                 send_msgs.push(msg);
             } else if let Some(recipient_ibc_channel_id) = &undelegation.recipient_ibc_channel_id {
                 if let Some(recipient) = &undelegation.recipient {
