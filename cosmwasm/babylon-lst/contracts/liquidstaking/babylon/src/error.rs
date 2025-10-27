4244a1562615311584c3fd163e9f93b3694ab6a3
Nuno David <ndavidq0@gmail.com>
2025-10-27 17:20:45 +0000

    feat: validate hex properly in validate_recipient; add validate_hex and validate_salt

--- cosmwasm/babylon-lst/contracts/liquidstaking/babylon/src/error.rs
+++ cosmwasm/babylon-lst/contracts/liquidstaking/babylon/src/error.rs
@@ -134,7 +134,7 @@ pub enum ContractError {
     #[error("invalid/unsupported recipient ibc channel id")]
     InvalidIBCChannelId {},
 
-    #[error("invalid {kind} address: {address} because: {reason}")]
+    #[error("invalid {kind} address/hex: {address} because: {reason}")]
     InvalidAddress {
         kind: String,
         address: String,
@@ -158,9 +158,6 @@ pub enum ContractError {
 
     #[error("no salt")]
     NoSalt,
-
-    #[error("invalid salt")]
-    InvalidSalt(#[from] unionlabs_primitives::encoding::HexPrefixedFromStrError),
 
     #[error("batch received amount: {received_amount} exceed {expected_native_unstaked}")]
     SlashBatchReceivedAmountExceedExpected {
