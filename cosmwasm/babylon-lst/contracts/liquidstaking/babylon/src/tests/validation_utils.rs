5f1f7f118770435c65f2cb2cb5fdf3da9144affa
Nuno David <ndavidq0@gmail.com>
2025-10-27 18:10:22 +0000

    test: cover validate_recipient zkgm kind

--- cosmwasm/babylon-lst/contracts/liquidstaking/babylon/src/tests/validation_utils.rs
+++ cosmwasm/babylon-lst/contracts/liquidstaking/babylon/src/tests/validation_utils.rs
@@ -2,7 +2,7 @@ use cosmwasm_std::testing::mock_dependencies;
 
 use crate::{
     ContractError,
-    state::{QuoteToken, Validator},
+    state::{CHAINS, Chain, QuoteToken, Validator},
     utils::validation::{self, *},
 };
 
@@ -143,4 +143,91 @@ fn test_validate_recipient_on_chain_recipient() {
     )
     .unwrap();
     assert_eq!(on_chain_recipient, true);
+}
+
+#[test]
+fn test_validate_recipient_channel_id() {
+    let mut deps = mock_dependencies();
+    let channel_id = 1;
+
+    let chain = Chain {
+        prefix: "cosmwasm".to_string(),
+        name: "chain".to_string(),
+        chain_id: "chain-1".to_string(),
+        ucs03_channel_id: channel_id,
+    };
+    CHAINS.save(&mut deps.storage, channel_id, &chain).unwrap();
+
+    let recipient = "0xeeEEeeE98622c19Ea39Ea8827ae22Bbfc732671c";
+    let salt = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+
+    // unknown channel_id
+    assert!(
+        validation::validate_recipient(
+            &deps.as_mut(),
+            Some(recipient.to_string()),
+            Some(5),
+            None,
+            &Some(salt.to_string()),
+        )
+        .is_err()
+    );
+
+    // missing recipient
+    assert!(
+        validation::validate_recipient(
+            &deps.as_mut(),
+            None,
+            Some(channel_id),
+            None,
+            &Some(salt.to_string()),
+        )
+        .is_err()
+    );
+
+    // invalid recipient
+    assert!(
+        validation::validate_recipient(
+            &deps.as_mut(),
+            Some("invalid".to_string()),
+            Some(channel_id),
+            None,
+            &Some(salt.to_string()),
+        )
+        .is_err()
+    );
+
+    // missing salt
+    assert!(
+        validation::validate_recipient(
+            &deps.as_mut(),
+            Some(recipient.to_string()),
+            Some(channel_id),
+            None,
+            &None,
+        )
+        .is_err()
+    );
+
+    // invalid salt
+    assert!(
+        validation::validate_recipient(
+            &deps.as_mut(),
+            Some(recipient.to_string()),
+            Some(channel_id),
+            None,
+            &Some("salt".to_string()),
+        )
+        .is_err()
+    );
+
+    let on_chain_recipient = validation::validate_recipient(
+        &deps.as_mut(),
+        Some(recipient.to_string()),
+        Some(channel_id),
+        None,
+        &Some(salt.to_string()),
+    )
+    .unwrap();
+    assert_eq!(on_chain_recipient, false);
 }
