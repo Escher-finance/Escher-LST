fa05377dc05b186c61546bd43cae1ea9211a75ca
Nuno David <ndavidq0@gmail.com>
2025-10-28 17:27:20 +0000

    test: cover migrate_reward execute fails if sender is not owner

--- cosmwasm/babylon-lst/contracts/liquidstaking/babylon/src/tests/execute.rs
+++ cosmwasm/babylon-lst/contracts/liquidstaking/babylon/src/tests/execute.rs
@@ -937,3 +937,26 @@ fn test_update_quote_token_must_fail_if_sender_not_owner() {
         ContractError::Ownership(cw_ownable::OwnershipError::NotOwner)
     ))
 }
+
+#[test]
+fn test_migrate_reward_must_fail_if_sender_not_owner() {
+    let mut deps = mock_dependencies();
+    let env = mock_env();
+    let api = deps.api.clone();
+    let sender = api.addr_make("sender");
+    let info = message_info(&sender, &[]);
+
+    cw_ownable::initialize_owner(
+        deps.as_mut().storage,
+        &api,
+        Some(api.addr_make("owner").as_str()),
+    )
+    .unwrap();
+
+    let err = migrate_reward(deps.as_mut(), env, info, 0).unwrap_err();
+
+    assert!(matches!(
+        err,
+        ContractError::Ownership(cw_ownable::OwnershipError::NotOwner)
+    ))
+}
