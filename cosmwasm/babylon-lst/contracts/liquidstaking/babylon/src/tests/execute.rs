54199be9cea1016e7bf1315488949b0be8260912
Nuno David <ndavidq0@gmail.com>
2025-10-28 17:21:05 +0000

    test: cover process_batch_withdrawal execute fails if sender is not owner

--- cosmwasm/babylon-lst/contracts/liquidstaking/babylon/src/tests/execute.rs
+++ cosmwasm/babylon-lst/contracts/liquidstaking/babylon/src/tests/execute.rs
@@ -857,3 +857,25 @@ fn test_set_parameters_must_fail_if_sender_not_owner() {
         ContractError::Ownership(cw_ownable::OwnershipError::NotOwner)
     ))
 }
+
+fn test_process_batch_withdrawal_must_fail_if_sender_not_owner() {
+    let mut deps = mock_dependencies();
+    let env = mock_env();
+    let api = deps.api.clone();
+    let sender = api.addr_make("sender");
+    let info = message_info(&sender, &[]);
+
+    cw_ownable::initialize_owner(
+        deps.as_mut().storage,
+        &api,
+        Some(api.addr_make("owner").as_str()),
+    )
+    .unwrap();
+
+    let err = process_batch_withdrawal(deps.as_mut(), env, info, 0, vec![]).unwrap_err();
+
+    assert!(matches!(
+        err,
+        ContractError::Ownership(cw_ownable::OwnershipError::NotOwner)
+    ))
+}
