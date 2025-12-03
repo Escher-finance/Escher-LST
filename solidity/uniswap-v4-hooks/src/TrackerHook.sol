75597cd6fa233c1e79297475dfbc9eb4b5a5b4e0
Nuno David <ndavidq0@gmail.com>
2025-12-03 16:45:53 +0000

    fix: correct implementations

--- solidity/uniswap-v4-hooks/src/TrackerHook.sol
+++ solidity/uniswap-v4-hooks/src/TrackerHook.sol
@@ -1,6 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.24;
 
 import {BaseHook} from "univ4-periphery/utils/BaseHook.sol";
+import {IPoolManager} from "univ4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "univ4-core/libraries/Hooks.sol";
 
-contract TrackerHook is BaseHook {}
+contract TrackerHook is BaseHook {
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: false,
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+}
