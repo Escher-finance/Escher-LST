2977eeb2467d2a3e7e85f10bfa509fd1a83b9ec8
Nuno David <ndavidq0@gmail.com>
2025-12-04 12:32:54 +0000

    fix: correct import conflicts

--- solidity/uniswap-v4-hooks/src/TrackerHook.sol
+++ solidity/uniswap-v4-hooks/src/TrackerHook.sol
@@ -10,7 +10,6 @@ import {
     ModifyLiquidityParams
 } from "univ4-periphery/utils/BaseHook.sol";
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
-import {PoolId, PoolIdLibrary} from "univ4-core/types/PoolId.sol";
 
 /// @dev Original event from `IPoolManager` with changed name
 /// @notice Emitted when a liquidity position is modified
@@ -21,7 +20,7 @@ import {PoolId, PoolIdLibrary} from "univ4-core/types/PoolId.sol";
 /// @param liquidityDelta The amount of liquidity that was added or removed
 /// @param salt The extra data to make positions unique
 event TrackedModifyLiquidity(
-    PoolId indexed id, address indexed sender, int24 tickLower, int24 tickUpper, int256 liquidityDelta, bytes32 salt
+    bytes32 indexed id, address indexed sender, int24 tickLower, int24 tickUpper, int256 liquidityDelta, bytes32 salt
 );
 
 interface IMsgSender {
@@ -32,6 +31,11 @@ contract TrackerHook is BaseHook, Ownable2Step {
     constructor(address _owner, IPoolManager _poolManager) BaseHook(_poolManager) Ownable(_owner) {}
 
     mapping(address => bool) public s_verifiedRouters;
+
+    /// @dev Equivalent to PoolId.sol from v4-core
+    function _getPoolId(PoolKey calldata key) internal returns (bytes32) {
+        return keccak256(abi.encode(key));
+    }
 
     function _getRealSender(address sender) internal returns (address) {
         if (s_verifiedRouters[sender]) {
@@ -77,9 +81,13 @@ contract TrackerHook is BaseHook, Ownable2Step {
         BalanceDelta feesAccrued,
         bytes calldata hookData
     ) internal override returns (bytes4, BalanceDelta) {
-        address _sender = _getRealSender(sender);
         emit TrackedModifyLiquidity(
-            PoolIdLibrary.toId(key), _sender, params.tickLower, params.tickUpper, params.liquidityDelta, params.salt
+            _getPoolId(key),
+            _getRealSender(sender),
+            params.tickLower,
+            params.tickUpper,
+            params.liquidityDelta,
+            params.salt
         );
         return (BaseHook.afterAddLiquidity.selector, delta);
     }
@@ -92,9 +100,13 @@ contract TrackerHook is BaseHook, Ownable2Step {
         BalanceDelta feesAccrued,
         bytes calldata hookData
     ) internal override returns (bytes4, BalanceDelta) {
-        address _sender = _getRealSender(sender);
         emit TrackedModifyLiquidity(
-            PoolIdLibrary.toId(key), _sender, params.tickLower, params.tickUpper, params.liquidityDelta, params.salt
+            _getPoolId(key),
+            _getRealSender(sender),
+            params.tickLower,
+            params.tickUpper,
+            params.liquidityDelta,
+            params.salt
         );
         return (BaseHook.afterRemoveLiquidity.selector, delta);
     }
