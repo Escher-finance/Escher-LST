d7828ef78a0950aa0531a2420780d742ea6ff6bc
Nuno David <ndavidq0@gmail.com>
2025-12-04 17:49:32 +0000

    feat: add position manager interface

--- solidity/il-solver/src/IlSolver.sol
+++ solidity/il-solver/src/IlSolver.sol
@@ -2,7 +2,14 @@
 pragma solidity ^0.8.24;
 
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {IPositionManager, PoolKey} from "univ4-periphery/interfaces/IPositionManager.sol";
 
 contract IlSolver is Ownable2Step {
-    constructor(address _owner) Ownable(_owner) {}
+    IPositionManager public s_posm;
+    PoolKey s_poolKey;
+
+    constructor(address _owner, IPositionManager _posm, PoolKey memory _poolKey) Ownable(_owner) {
+        s_posm = _posm;
+        s_poolKey = _poolKey;
+    }
 }
