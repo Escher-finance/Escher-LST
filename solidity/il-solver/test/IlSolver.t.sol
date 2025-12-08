3e70a0ac691aed0416183b3913839e68bbe87efd
Nuno David <ndavidq0@gmail.com>
2025-12-08 14:42:23 +0000

    test: setup balances and assert pool is good in fork

--- solidity/il-solver/test/IlSolver.t.sol
+++ solidity/il-solver/test/IlSolver.t.sol
@@ -4,19 +4,26 @@ pragma solidity ^0.8.24;
 import {Test, console} from "forge-std/Test.sol";
 import {IlSolver, IPositionManager, PoolKey, Currency, CurrencyLibrary} from "../src/IlSolver.sol";
 import {IHooks} from "univ4-core/interfaces/IHooks.sol";
+import {IPoolManager} from "univ4-core/interfaces/IPoolManager.sol";
+import {IStateView, PoolId} from "univ4-periphery/interfaces/IStateView.sol";
 
 using CurrencyLibrary for Currency;
 
 contract IlSolverTest is Test {
     IlSolver c;
-    PoolKey key;
     address owner;
+    PoolKey key;
+    IPoolManager poolManager;
+    IStateView stateView;
 
     function setUp() public {
         vm.createSelectFork("mainnet", 23968000);
         owner = makeAddr("owner");
 
+        // https://docs.uniswap.org/contracts/v4/deployments
         IPositionManager posm = IPositionManager(0xbD216513d74C8cf14cf4747E6AaA6420FF64ee9e);
+        poolManager = IPoolManager(0x000000000004444c5dc75cB358380D2e3dE08A90);
+        stateView = IStateView(0x7fFE42C4a5DEeA5b0feC41C94C136Cf115597227);
 
         key = PoolKey({
             currency0: CurrencyLibrary.ADDRESS_ZERO,
@@ -25,8 +32,8 @@ contract IlSolverTest is Test {
             tickSpacing: 60,
             hooks: IHooks(address(0))
         });
-        bytes32 keyId = bytes32(0xdce6394339af00981949f5f3baf27e3610c76326a700af57e4b3e3ae4977f78d);
-        assertEq(keccak256(abi.encode(key)), keyId);
+        bytes32 id = bytes32(0xdce6394339af00981949f5f3baf27e3610c76326a700af57e4b3e3ae4977f78d);
+        assertEq(keccak256(abi.encode(key)), id);
 
         c = new IlSolver(owner, posm, key);
         assertEq(c.owner(), owner);
@@ -37,6 +44,8 @@ contract IlSolverTest is Test {
 
         assertGt(key.currency0.balanceOf(owner), 0);
         assertGt(key.currency1.balanceOf(owner), 0);
+        uint128 liquidity = stateView.getLiquidity(PoolId.wrap(id));
+        assertGt(liquidity, 0);
     }
 
     function testUniV4Mint() public pure {
