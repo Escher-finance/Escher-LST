9074b2339bb407ce2e80c0d8c03477075b3f0d96
Isak Rickyanto <isak.rickyanto@gmail.com>
2025-12-22 17:21:54 +0700

    Set solidity to 0.8.28

--- solidity/evm-lst/src/tokens/Lst.sol
+++ solidity/evm-lst/src/tokens/Lst.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.28;
+pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
