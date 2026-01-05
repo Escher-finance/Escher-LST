8cb8ba8b452f43a7b8be60ad17ba2264fae7a177
Nuno David <ndavidq0@gmail.com>
2026-01-05 11:38:18 +0000

    test: refactor variable names

--- solidity/il-solver/test/IlSolver.t.sol
+++ solidity/il-solver/test/IlSolver.t.sol
@@ -153,11 +153,12 @@ contract IlSolverTest is Test {
 
     function testUniV4SwapZeroForOne() public {
         uint256 usdcBalanceOld = collateral.balanceOf(address(c));
-        uint128 amount0 = 1 ether;
-        uint256 amount1 = c.univ4Swap(true, amount0, 1);
+        uint128 ethIn = 1 ether;
+        uint128 minUsdcOut = 3300e6;
+        uint256 amount1 = c.univ4Swap(true, ethIn, minUsdcOut);
         uint256 usdcBalanceNew = collateral.balanceOf(address(c));
         assertEq(usdcBalanceNew - usdcBalanceOld, amount1);
-        assertGt(amount1, 3300e6);
+        assertGt(amount1, minUsdcOut);
     }
 
     function testAaveV3Supply() public {
