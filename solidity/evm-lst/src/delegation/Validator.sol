b5f1154053fa44d614c0b9a0a45e448e936398fb
Isak Rickyanto <isak.rickyanto@gmail.com>
2026-01-13 13:37:50 +0700

    Remove unused Validator file

--- solidity/evm-lst/src/delegation/Validator.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.28;
-
-import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import {IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract Validator {}
