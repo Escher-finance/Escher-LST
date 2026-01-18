fa1c1e6e4a951489b346169108008f89c423dcc9
Nuno David <ndavidq0@gmail.com>
2026-01-18 21:15:15 +0000

    feat: add looping logic

--- solidity/il-solver/src/IlSolverHook.sol
+++ solidity/il-solver/src/IlSolverHook.sol
@@ -5,7 +5,10 @@ import {BaseHook, Hooks, IPoolManager, ModifyLiquidityParams, BalanceDelta} from
 import {BalanceDeltaLibrary} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
 import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {IV4Router} from "@uniswap/v4-periphery/src/interfaces/IV4Router.sol";
+import {Actions} from "@uniswap/v4-periphery/src/libraries/Actions.sol";
 import {IlSolverMath} from "./core/EscherMath.sol";
+import {Currency, CurrencyLibrary} from "@uniswap/v4-core/src/types/Currency.sol";
 import {IAaveOracle} from "aavev3/interfaces/IAaveOracle.sol";
 import {DataTypes} from "aavev3/protocol/libraries/types/DataTypes.sol";
 import {IL2Pool as IL2PoolOriginal} from "aavev3/interfaces/IL2Pool.sol";
@@ -18,6 +21,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
+using CurrencyLibrary for Currency;
 using ReserveConfiguration for DataTypes.ReserveConfigurationMap;
 using SafeCast for int128;
 
@@ -25,6 +29,56 @@ interface IL2Pool is IL2PoolOriginal, IPool {}
 
 interface IMsgSender {
     function msgSender() external view returns (address);
+}
+
+interface IUniversalRouter {
+    function execute(bytes calldata commands, bytes[] calldata inputs, uint256 deadline) external payable;
+}
+
+// src: https://github.com/Uniswap/universal-router/blob/main/contracts/libraries/Commands.sol
+library UniversalRouterCommands {
+    // Masks to extract certain bits of commands
+    bytes1 internal constant FLAG_ALLOW_REVERT = 0x80;
+    bytes1 internal constant COMMAND_TYPE_MASK = 0x7f;
+
+    // Command Types. Maximum supported command at this moment is 0x3f.
+    // The commands are executed in nested if blocks to minimise gas consumption
+
+    // Command Types where value<=0x07, executed in the first nested-if block
+    uint256 constant V3_SWAP_EXACT_IN = 0x00;
+    uint256 constant V3_SWAP_EXACT_OUT = 0x01;
+    uint256 constant PERMIT2_TRANSFER_FROM = 0x02;
+    uint256 constant PERMIT2_PERMIT_BATCH = 0x03;
+    uint256 constant SWEEP = 0x04;
+    uint256 constant TRANSFER = 0x05;
+    uint256 constant PAY_PORTION = 0x06;
+    // COMMAND_PLACEHOLDER = 0x07;
+
+    // Command Types where 0x08<=value<=0x0f, executed in the second nested-if block
+    uint256 constant V2_SWAP_EXACT_IN = 0x08;
+    uint256 constant V2_SWAP_EXACT_OUT = 0x09;
+    uint256 constant PERMIT2_PERMIT = 0x0a;
+    uint256 constant WRAP_ETH = 0x0b;
+    uint256 constant UNWRAP_WETH = 0x0c;
+    uint256 constant PERMIT2_TRANSFER_FROM_BATCH = 0x0d;
+    uint256 constant BALANCE_CHECK_ERC20 = 0x0e;
+    // COMMAND_PLACEHOLDER = 0x0f;
+
+    // Command Types where 0x10<=value<=0x20, executed in the third nested-if block
+    uint256 constant V4_SWAP = 0x10;
+    uint256 constant V3_POSITION_MANAGER_PERMIT = 0x11;
+    uint256 constant V3_POSITION_MANAGER_CALL = 0x12;
+    uint256 constant V4_INITIALIZE_POOL = 0x13;
+    uint256 constant V4_POSITION_MANAGER_CALL = 0x14;
+    // COMMAND_PLACEHOLDER = 0x15 -> 0x20
+
+    // Command Types where 0x21<=value<=0x3f
+    uint256 constant EXECUTE_SUB_PLAN = 0x21;
+    // COMMAND_PLACEHOLDER for 0x22 to 0x3f
+
+    // Command Types where 0x40<=value<=0x5f
+    // Reserved for 3rd party integrations
+    uint256 constant ACROSS_V4_DEPOSIT_V3 = 0x40;
 }
 
 struct UserData {
@@ -52,18 +106,31 @@ contract IlSolverHook is BaseHook, Ownable2Step {
     // The collateral asset (e.g. USDC)
     IERC20 public immutable COLLATERAL;
 
+    // Must have `currency0` set to ETH and `currency1` set to `collateral`
+    // Used in swap
+    PoolKey public uniPoolKey;
+    IUniversalRouter public uniRouter;
+
     constructor(
         address _owner,
         IPoolManager _poolManager,
         IWETH _weth,
         IERC20 _collateral,
+        PoolKey memory _uniPoolKey,
+        IUniversalRouter _uniRouter,
         IL2Pool _aavePool,
         L2Encoder _aaveEncoder,
         IAaveOracle _aaveOracle
     ) BaseHook(_poolManager) Ownable(_owner) {
         users[_owner] = true;
+
+        require(Currency.unwrap(_uniPoolKey.currency0) == address(0));
+        require(Currency.unwrap(_uniPoolKey.currency1) == address(_collateral));
+        uniPoolKey = _uniPoolKey;
+
         WETH = _weth;
         COLLATERAL = _collateral;
+        uniRouter = _uniRouter;
         aavePool = _aavePool;
         aaveOracle = _aaveOracle;
         aaveEncoder = _aaveEncoder;
@@ -242,12 +309,62 @@ contract IlSolverHook is BaseHook, Ownable2Step {
         aavePool.borrow(address(WETH), amount, 2, 0, address(this));
     }
 
+    /**
+     * @dev Swaps exact input single from the Uniswap V4 pool with `uniPoolKey`
+     * @dev Swaps one of the tokens for the other configured via `zeroForOne`
+     * @return actualAmountOut Actual amount returned after the swap (i.e. amount1 returned to the contract if zeroForOne is true)
+     */
+    function _univ4Swap(bool zeroForOne, uint128 amountIn, uint128 minAmountOut)
+        private
+        // univ4AttachFundsForSwap(zeroForOne, amountIn)
+        returns (uint256 actualAmountOut)
+    {
+        bytes memory commands = abi.encodePacked(uint8(UniversalRouterCommands.V4_SWAP));
+        bytes memory actions =
+            abi.encodePacked(uint8(Actions.SWAP_EXACT_IN_SINGLE), uint8(Actions.SETTLE_ALL), uint8(Actions.TAKE_ALL));
+
+        PoolKey memory key = uniPoolKey;
+        bytes[] memory params = new bytes[](3);
+
+        params[0] = abi.encode(
+            IV4Router.ExactInputSingleParams({
+                poolKey: key,
+                zeroForOne: zeroForOne,
+                amountIn: amountIn,
+                amountOutMinimum: minAmountOut,
+                hookData: bytes("")
+            })
+        );
+        params[1] = abi.encode(zeroForOne ? key.currency0 : key.currency1, amountIn);
+        params[2] = abi.encode(zeroForOne ? key.currency1 : key.currency0, minAmountOut);
+
+        bytes[] memory inputs = new bytes[](1);
+        inputs[0] = abi.encode(actions, params);
+        uint256 deadline = block.timestamp;
+
+        uint256 bBefore = (zeroForOne) ? key.currency1.balanceOfSelf() : key.currency0.balanceOfSelf();
+
+        uniRouter.execute{value: zeroForOne ? amountIn : 0}(commands, inputs, deadline);
+
+        uint256 bAfter = (zeroForOne) ? key.currency1.balanceOfSelf() : key.currency0.balanceOfSelf();
+
+        actualAmountOut = bAfter - bBefore;
+    }
+
     function loop() public payable onlyUser {
         UserData memory senderData = usersData[msg.sender];
         require(senderData.collateralAmountNeeded > 0 && !senderData.done);
+        uint256 iterations = senderData.iterations / 1e18;
 
+        uint256 collateralAmount = senderData.collateralAmountNeeded;
         // 1. supply usdc (collateral)
         // 2. borrow weth
         // 3. swap weth with usdc
+        for (uint256 i = 0; i < iterations; i++) {
+            _aavev3Supply(collateralAmount);
+            uint256 currentBorrowAmount = senderData.borrowedAmounts[i];
+            _aavev3Borrow(currentBorrowAmount);
+            collateralAmount = _univ4Swap(true, uint128(currentBorrowAmount), 0);
+        }
     }
 }
