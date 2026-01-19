a540456c63afde8683d215922b1a6853090d1687
Nuno David <ndavidq0@gmail.com>
2026-01-19 15:42:58 +0000

    refactor: remove console logs from contract

--- solidity/il-solver/src/IlSolverHook.sol
+++ solidity/il-solver/src/IlSolverHook.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {console} from "forge-std/Test.sol";
 import {BaseHook, Hooks, IPoolManager, ModifyLiquidityParams, BalanceDelta} from "v4-periphery/src/utils/BaseHook.sol";
 import {BalanceDeltaLibrary} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
 import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
@@ -412,8 +411,6 @@ contract IlSolverHook is BaseHook, Ownable2Step {
 
         uint256 collateralAmount = senderData.collateralAmountNeeded;
         COLLATERAL.transferFrom(msg.sender, address(this), collateralAmount);
-
-        console.log("transferred collateral", collateralAmount);
 
         // 1. supply usdc (collateral)
         // 2. borrow weth
